ff9ee0471f8bd0a577cfd34f726ce7b3f90b32be
Fernando G. Ponce <data.fernan@gmail.com>
2025-09-22 17:32:07 -0500

    como en el video

--- ejetcicio Git/utilitarios/saludo.js
+++ ejetcicio Git/utilitarios/saludo.js
@@ -1,34 +1,33 @@
 saludar=function(){
     let nombre=recuperarTexto("txtNombre")
     let apellido=recuperarTexto("txtApellido")
-    let edad=recuperarTexto("txtEdad")
-    let altura=recuperarTexto("txtAltura")
+    let edad=recuperarInt("txtEdad")
+    let altura=recuperarFloat("txtAltura")
+    console.log(typeof(edad)+" "+typeof(altura))
     
 }
 
 
 recuperarTexto=function(idComponente){
     let componente
-    let valorIngresado
+    let valorCaja
     componente=document.getElementById(idComponente)
-    valorIngresado=componente.value
-    return valorIngresado;
+    valorCaja=componente.value
+    return valorCaja;
 }
 
 
 
 recuperarInt=function(idComponente){
 
-    let compEntero;
-    compEntero=document.getElementById(idComponente)
-    let entero= parseInt(compEntero.value)
-    return entero
+    let compEntero=recuperarTexto(idComponente)
+    let valorCaja=parseInt(compEntero.value)
+    return valorCaja
 }
 
 recuperarFloat=function(idComponente){
 
-    let compFloat;
-    compFloat=document.getElementById(idComponente)
-    let float= parseFloat(compFloat.value)
-    return float
+    let compFloat=recuperarTexto(idComponente)
+    let valorCaja=parseInt(compFloat.value)
+    return valorCaja
 }
