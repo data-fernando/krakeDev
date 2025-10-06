a6640134d21ecf06610ad901f63e530af4fea849
Fernando G. Ponce <data.fernan@gmail.com>
2025-10-06 17:31:56 -0500

    reto 46 roles parte 2

--- ejercicio Git - Modulo 1/Roles/roles.js
+++ ejercicio Git - Modulo 1/Roles/roles.js
@@ -3,12 +3,15 @@ let empleados = [
     {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
 ]
 
-
+//PARTE 1
 mostrarOpcionEmpleado=function(){
     
     mostrarComponente("divEmpleado")
     ocultarComponente("divRol")
     ocultarComponente("divResumen")
+
+    //AGREGADO EN LA PARTE 2
+    mostarEmpleados()
     
 }
 
@@ -22,4 +25,30 @@ mostrarOpcionResumen=function(){
     mostrarComponente("divResumen")
     ocultarComponente("divEmpleado")
     ocultarComponente("divRol")
+}
+
+//PARTE 2 
+mostarEmpleados=function(){
+    
+    let compTabla=document.getElementById("tablaEmpleados")
+
+    let contenidoTabla="<table><tr> <th>CEDULA</th> <th>NOMBRE</th> <th>APELLIDO</th> <th>SUELDO</th> </tr>"
+    let elementoEmpleado
+
+    for(let i=0;i<empleados.length;i++){
+        elementoEmpleado=empleados[i]
+        contenidoTabla+=
+        "<tr>"+
+        "<td>"+elementoEmpleado.cedula+"</td>"+
+        "<td>"+elementoEmpleado.nombre+"</td>"+
+        "<td>"+elementoEmpleado.apellido+"</td>"+
+        "<td>"+elementoEmpleado.sueldo+"</td>"
+        +"</tr>"
+
+    }
+
+    contenidoTabla+="</table>"
+    compTabla.innerHTML=contenidoTabla
+
+
 }
