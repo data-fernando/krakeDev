47e956a842251394f0164b2eb56c86799e3d289d
Fernando G. Ponce <data.fernan@gmail.com>
2025-10-06 16:11:50 -0500

    reto 44 objetos clientes

--- ejercicio Git - Modulo 1/objetos/objetos.js
+++ ejercicio Git - Modulo 1/objetos/objetos.js
@@ -18,6 +18,95 @@ probarAtributos=function(){
 
 }
 
+
+modificarAtributos=function(){
+
+    let cuenta = {
+        numero: "5323423423",
+        saldo:0.0
+
+    }
+
+    cuenta.saldo=100
+    cuenta.saldo+=10
+    console.log("saldo de la cuenta es"+cuenta.saldo+" "+modificarAtributos.name)
+
+
+}
+
+crearCliente=function(){
+    let cliente={
+        nombre:"juan",
+        cedula:"1625489635"
+    }
+
+    let cliente1={}
+    cliente1.nombre="Romeo"
+    cliente1.cedula="0845963578"
+
+
+}
+
+probarincrementarSaldo=function(){
+    let cta={numero:"2325242659",saldo:34.0}
+    console.log(cta.saldo)
+
+    incrementarSaldo(cta,100)
+
+
+    console.log(cta.saldo)
+
+}
+
+probarDeterminarMayor=function(){
+    let perMayor
+    let per1={nombre:"Romeo",edad:45}
+    let per2={nombre:"Rosa",edad:40}
+
+    perMayor=determinarMayor(per1,per2)
+
+    if(perMayor==null){
+        console.log("ambos tienen la misma edad")
+    }else{
+        console.log("la persona mayor es: "+per1.nombre)
+    }
+
+
+
+
+}
+
+incrementarSaldo=function(cuenta,monto){
+        cuenta.saldo+=monto
+}
+
+determinarMayor=function(persona1,persona2){
+    let personaMayor
+
+    if(persona1.edad>persona2.edad){
+        personaMayor=persona1
+    }else if(persona1.edad<persona2.edad){
+        personaMayor=persona2
+    }else{
+        personaMayor=null
+    }
+
+    return personaMayor
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 crearProducto=function(){
     let producto1={
         nombre:"cuerdas de guitarra",
