c11749172794e6a0561a5f57aaef85aacc851939
Fernando G. Ponce <data.fernan@gmail.com>
2025-11-18 22:14:21 -0500

    Reto 30 Junit maven

---
+++ Java/Log4J/src/com/krakedev/ejercicios/Admin.java
@@ -0,0 +1,27 @@
+package com.krakedev.ejercicios;
+
+import org.apache.logging.log4j.LogManager;
+//importamos la de apache
+import org.apache.logging.log4j.Logger;
+
+public class Admin {
+	//log manager de apache
+//	private Logger logger=LogManager.getLogger(getClass());
+	private static Logger logger=LogManager.getLogger(Admin.class);
+	
+	
+	public void agregar() {
+		//orden ascendente
+	    logger.trace("Mensaje de trace");   // nivel más bajo
+	    logger.debug("Mensaje de debug");   // depuración
+	    logger.info("Mensaje de info");     // informativo
+	    logger.warn("Mensaje de warn");     // advertencia
+	    logger.error("Mensaje de error");   // error
+	    // logger.fatal("Mensaje de fatal"); // opcional, error crítico
+	    
+	    //dependiendo del xml es igual o superior al nivel que coloquemos
+	    //Root level="warn"
+	}
+
+
+}
---
+++ Java/Maven_Demostracion/java/com/krakedev/ejemploMavenFer/TestAdmin.java
@@ -0,0 +1,14 @@
+package com.krakedev.ejemploMavenFer;
+//corregir la invocacion del paquete
+
+public class TestAdmin {
+	
+	public static void main(String[] args) {
+		Admin admin=new Admin();
+		admin.agregar();
+	}
+	
+	//agregar el xml a sources
+	//calbiar el nombre a solo log4j2.xml
+	
+}
